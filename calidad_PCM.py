598c36ce98c10fd7ce474ae77c88090f7eb2edb6
Laura Mariana Jiménez <69278768+LauraMarianaJimenezJimenez@users.noreply.github.com>
2023-02-13 21:19:41 -0500

    Add files via upload

---
+++ calidad_PCM.py
@@ -0,0 +1,186 @@
+# Importe de librerias a utilizar
+import pandas as pd
+import numpy as np
+from pandas import ExcelWriter
+import datetime
+import time
+import warnings
+import os
+import re
+
+warnings.filterwarnings("ignore")
+
+try:
+	# Se solicita la fecha del archivo para la creación del path que leera el archivo
+
+	# Input
+	print("Inserte la fecha de la fuente que desea procesar (yyyymm)")
+	data_date = input()
+	print("Ingrese la tasa de cambio del mes separado por punto")
+	rate = float(input())
+	#20230116
+	#629,71
+
+	print('Procesando...')
+
+	# file = os.path.abspath("../Fuentes_iniciales/ManualTransactions_"+ data_date "_.xls")
+	file = os.path.abspath("../Fuentes_iniciales/PCM_" + data_date + ".xlsx")
+
+	# Raed file
+	df = pd.read_excel(file, header=None)
+
+	try:
+		# Se toman el dataframe desde la fila 8 de la fuente para descartar textos informativos
+
+		# Take dataframe
+		df.columns = df.iloc[0]
+		df = df[1:]
+
+		# Remove entirely empty row
+		df = df.dropna(how='all')
+		# Remove entirely empty column
+		df = df.dropna(how='all', axis=1)
+		try:
+			# Delete withespace in headers
+			df = df.rename(columns=lambda x: x.strip())
+			# Take specific columns
+			df = df[['Fecha', 'Ente', 'Comisión MO', 'Dolarizado', 'Moneda', 'Causa', 'Tipo Comisión', 'Producto Vista # cuenta', 'Nombre producto']]
+			# Remove duplicate records
+			df = df.drop_duplicates()
+			try: 
+				path = os.path.abspath('../Informes/Informe_PCM_202212.txt')
+
+				f = open(path,"w+")
+				f.write(file)
+
+				# Print columns and rows
+				f.write("\nCantidad de filas: %d" % len(df))
+				f.write("\nCantidad de Columnas: %d" % len(df.columns))
+
+				f.write("\nCantidad de datos vacios por cada columna del archivo")
+
+				# Validate empty cells
+				for column in df:
+					text = str(column)
+					f.write("\n")
+					f.write(text)
+					f.write(": ")
+					text = str(df[column].isnull().sum())
+					f.write(text)
+					df[column] = df[column].astype(str)
+
+				#Remove carring return
+				df = df.replace({r'\\r': ' '}, regex=True)
+				#Remove line breaks
+				df = df.replace(r'\s+|\\n', ' ', regex=True)
+				#Remove pipelines, single quote, semicolon
+				df = df.replace(r'\| +|\' +|; +|´ +|\|', '', regex=True)
+
+
+				i = 0
+				for column in df:
+					# Fecha 
+					# Formato dd/mm/yyyy y se valida que todos los meses correspondan a 
+					if i == 0:
+						df[column] = np.where(df[column].str.contains('/'), pd.to_datetime(df[column], errors='coerce').dt.strftime('%d/%m/%Y'), pd.to_datetime(df[column], errors='coerce', dayfirst=True).dt.strftime('%d/%m/%Y'))
+						df[column] = df[column].astype(str)
+						if (df[column].str.slice(3, 5)  != data_date[4:6]).any():
+							f.write("\nHay fechas que no corresponden para el mes de ejecución")
+
+					# Ente
+					# Solo datos numéricos
+					if i == 1:
+						df[column] = df[column].astype(str)
+						df[column] = df[column].str.replace('[^0-9\\s]+', '', regex=True)
+
+					# Comisión MO
+					# Solo datos numéricos, decimal separado por punto
+					if i == 2:
+						df[column] = df[column].astype(str)
+						df[column] = df[column].str.replace('[^0-9,.\\s]+', '', regex=True)
+						df[column] = df[column].str.replace(',', '.', regex=False)
+						df[column] = df[column].astype(float)
+
+					# Dolarizado
+					# 
+					if i == 3:
+						for items in df['Moneda'].iteritems():
+							if(items[1] == '20'):
+								dolar = df.loc[items[0]][2] / rate
+								df.loc[items[0]][3] = dolar
+							
+							if(items[1] == '0'):
+								dolar = df.loc[items[0]][2]
+								df.loc[items[0]][3] = dolar
+
+							if(items[1] != '0' or items[1] != '20'):
+								df.loc[items[0]][3] = 0
+
+						df[column] = df[column].str.replace('[^0-9,.\\s]+', '', regex=True)
+
+
+					# Moneda
+					# Solo valor 0 y valor 20
+					if i == 4:
+						monedas = ['0', '20']
+						if (~df[column].isin(monedas).any()):
+							f.write("\nHay monedas que no corresponden a 0 o 20")
+
+					# Causa
+					# Solo datos numéricos
+					if i == 5:
+						df[column] = df[column].astype(str)
+						df[column] = df[column].str.replace('[^0-9\\s]+', '', regex=True)
+
+					# Tipo Comision
+					# a zA Z
+					if i == 6:
+						df[column] = df[column].astype(str)
+						df[column] = df[column].str.replace('[^a-zA-Z\\s]+', '', regex=True)
+
+					# Producto Vista # cuenta
+					if i == 7:
+						df[column] = df[column].astype(str)
+						if (df[column] == 'ND').any():
+							f.write("\nHay valores ND en columna Producto Vista # cuenta")
+							df[column] = df[column].str.replace('ND', '', regex=False)
+
+					# Nombre Producto
+					if i == 8:
+						df[column] = df[column].astype(str)
+						if (df[column] == 'ND').any():
+							f.write("\nHay valores ND en columna Producto Vista # cuenta")
+							df[column] = df[column].str.replace('ND', '', regex=False)
+						df[column] = df[column].str.replace('[^a-zA-Z-\\s]+', '', regex=True)
+
+
+					i = i + 1
+
+				f.close()
+
+				# Replace nan to empty
+				df = df.fillna('')
+				df = df.replace('nan', '', regex=False)
+
+				# Generación del flag de validación, marcación de tiempo unix
+				date_time = datetime.datetime.now()      
+				unix_time = time.mktime(date_time.timetuple())
+				unix_time = str(unix_time)
+				unix_time = unix_time.split('.')[0]
+
+				# Se escribe un nuevo archivo con la fuente procesada 
+
+				file = os.path.abspath('../Fuentes_procesadas/PCM_' + data_date + '_' + unix_time + '.xlsx')
+				writer = ExcelWriter(file)
+				df.to_excel(writer, 'PCM', index=False)
+				writer.save()
+
+				print("Fuentes procesada con exito")
+			except:
+				print(' Ha ocurrido un error, por favor verifique su fuente')
+		except:
+			print(' Hay un error en los nombres de las columnas, valide que sean [en_ente, en_linea_neg_cv, en_subsegmento, ah_cta_banco, hm_moneda, hm_fecha, hm_valor, hm_referencia, hm_signo], teniendo en cuenta el orden, las mayusculas y minusculas')
+	except:
+		print(' Ha ocurrido un error, por favor verifique su fuente')
+except:
+	print(" Hay un error en la fecha ingresada o en el nombre del archivo")
